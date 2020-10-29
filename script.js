c62a4bc672c8b420a01f604e658e61d8cc1a52ec
Pratham0502 <72384535+Pratham0502@users.noreply.github.com>
2020-10-29 09:52:21 +0530

    changes

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ $(function () {
     }
   });
   });
-
-  function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+  // Read More
+  // function myFunction() {
+  //   var dots = document.getElementById("dots");
+  //   var moreText = document.getElementById("more");
+  //   var btnText = document.getElementById("myBtn");
   
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
-  }
+  //   if (dots.style.display === "none") {
+  //     dots.style.display = "inline";
+  //     btnText.innerHTML = "Read more";
+  //     moreText.style.display = "none";
+  //   } else {
+  //     dots.style.display = "none";
+  //     btnText.innerHTML = "Read less";
+  //     moreText.style.display = "inline";
+  //   }
+  // }
