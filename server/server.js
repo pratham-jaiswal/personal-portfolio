04d6a410aa228136a5c243ac04e92c1640a79f01
pratham-jaiswal <prathamj0502@gmail.com>
2023-09-09 12:45:19 +0530

    Add default cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
