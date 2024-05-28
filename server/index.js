e69c7da15cb9dfdbc419a783b77c066d9f470b84
pratham-jaiswal <prathamj0502@gmail.com>
2024-05-28 19:09:46 +0530

    just code formatting

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const corsMiddleware = require('./cors');
+const corsMiddleware = require("./cors");
 const app = express();
 require("dotenv").config();
 
@@ -10,60 +10,61 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public", { maxAge: 30 * 24 * 60 * 60 * 1000 }));
 app.use(compression());
 
-mongoose.connect(`${process.env.MONGODB_URI}/portfolioDB`).then(() => {
-
+mongoose
+  .connect(`${process.env.MONGODB_URI}/portfolioDB`)
+  .then(() => {
     const descSchema = {
-        name: {
+      name: {
         type: String,
         required: true,
-        },
-        content: {
-            type: String,
-            required: true,
-        },
+      },
+      content: {
+        type: String,
+        required: true,
+      },
     };
 
     const Desc = mongoose.model("Description", descSchema);
 
     const skillsSchema = {
-        name: {
+      name: {
         type: String,
         required: true,
-        },
-        shield: {
-            type: String,
-            required: true,
-        },
+      },
+      shield: {
+        type: String,
+        required: true,
+      },
     };
 
     const Skill = mongoose.model("Skill", skillsSchema);
 
     const projectsSchema = {
-        codeLink: String,
-        title: {
+      codeLink: String,
+      title: {
         type: String,
         required: true,
-        },
-        banner: {
-            type: String,
-            required: true,
-        },
-        technologies: {
-            type: [String],
-            required: true,
-        },
-        features: {
-            type: [String],
-            required: true,
-        },
-        category: {
-            type: String,
-            required: true,
-        },
-        date: {
-            type: Date,
-            required: true,
-        }
+      },
+      banner: {
+        type: String,
+        required: true,
+      },
+      technologies: {
+        type: [String],
+        required: true,
+      },
+      features: {
+        type: [String],
+        required: true,
+      },
+      category: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
     };
 
     const Project = mongoose.model("Project", projectsSchema);
@@ -71,42 +72,44 @@ mongoose.connect(`${process.env.MONGODB_URI}/portfolioDB`).then(() => {
     const port = process.env.PORT || 3001;
 
     app.get("/api/data", corsMiddleware, async (req, res) => {
-        try {
-            const allDesc = await Desc.find();
-            const allSkills = await Skill.find();
-            const allProjects = await Project.find().sort({ date: -1 });
-            return res.json({ desc: allDesc, skills: allSkills, projects: allProjects });
-        }
-        catch (err) {
-            console.log(err);
-            return res.status(500).json({ error: "Internal server error" });
-        }
+      try {
+        const allDesc = await Desc.find();
+        const allSkills = await Skill.find();
+        const allProjects = await Project.find().sort({ date: -1 });
+        return res.json({
+          desc: allDesc,
+          skills: allSkills,
+          projects: allProjects,
+        });
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Internal server error" });
+      }
     });
 
     app.get("/", async (req, res) => {
-        try {
-            res.send("Server is running");
-        }
-        catch (err) {
-            console.log(err);
-        }
+      try {
+        res.send("Server is running");
+      } catch (err) {
+        console.log(err);
+      }
     });
 
     app.get("/api/cohereKey", async (req, res) => {
-        try {
-            const cohereKey = process.env.COHERE_API_KEY;
-            res.json({ cohereKey });
-        }
-        catch (err) {
-            console.log(err);
-        }
+      try {
+        const cohereKey = process.env.COHERE_API_KEY;
+        res.json({ cohereKey });
+      } catch (err) {
+        console.log(err);
+      }
     });
 
     app.listen(port, () => {
-        console.log("Server started on port "+port);
+      console.log("Server started on port " + port);
     });
-}).catch(err => {
+  })
+  .catch((err) => {
     console.error("MongoDB connection error:", err);
-});
+  });
 
 module.exports = app;
